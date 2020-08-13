7c64fe1488d9566f73ad3f6bfd289adc86c865e5
David M. Johnson <johnsond@flux.utah.edu>
2020-08-13 17:29:53 -0600

    Add a "swapout" hook that revokes letsencrypt certs to avoid later email spam.

--- setup-letsencrypt.sh
+++ setup-letsencrypt.sh
@@ -22,9 +22,33 @@ logtstart "letsencrypt"
 maybe_install_packages python-certbot-nginx
 certbot certonly -d $NFQDN --nginx --agree-tos -m "$SWAPPER_EMAIL" -n
 mkdir -p /etc/nginx/ssl
-cp -p /etc/letsencrypt/live/$NFQDN/*.pem /etc/nginx/ssl/
-chown -R www-data:root /etc/nginx/ssl/
-chmod 770 /etc/nginx/ssl
+#cp -p /etc/letsencrypt/live/$NFQDN/*.pem /etc/nginx/ssl/
+#chown -R www-data:root /etc/nginx/ssl/
+#chmod 770 /etc/nginx/ssl
+
+#
+# Add a simple revocation service that runs on shutdown/reboot and if
+# the node is no longer allocated, certbot revoke .
+#
+cat <<'EOF' >/etc/systemd/system/tbhook.service
+[Unit]
+Description=Testbed Hook Service
+After=testbed.service
+
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/bin/sh -c "/usr/local/etc/emulab/tmcc.bin -s boss status > /var/emulab/boot/tmcc/status"
+ExecStop=/bin/sh -c '. /var/emulab/boot/tmcc/status ; if [ -z "$ALLOCATED" ]; then echo "No info from tmcd, skipping" ; exit 0 ; fi ; OLD="$ALLOCATED" ; /usr/local/etc/emulab/tmcc.bin -s boss status > /tmp/allocated ; . /tmp/allocated ; if [ -z "$ALLOCATED" ]; then echo "No updated info from tmcd, skipping" ; exit 0 ; fi ; if [ "$OLD" = "$ALLOCATED" ]; then exit 0 ; fi ; echo Revoking "letsencrypt certificate"; certbot revoke --cert-path /etc/letsencrypt/live/`cat /var/emulab/boot/nodeid`.`cat /var/emulab/boot/mydomain`/fullchain.pem -n ; exit 0'
+StandardOutput=journal+console
+StandardError=journal+console
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daemon-reload
+systemctl enable tbhook
+systemctl start tbhook
 
 logtend "letsencrypt"
 touch $OURDIR/letsencrypt-done
