cad36dc23eaf5b269a27dd2c8e85e514a0ac173f
Zhaoqi(Roy) Zhang <Zhaoqi.Zhang@tufts.edu>
2024-07-20 20:42:10 -0400

    Update user permission in dsb script

--- setup-dsb.sh
+++ setup-dsb.sh
@@ -20,16 +20,15 @@ echo "setup deathstarbench in k8s"
 # Clone the repository as geniuser
 cd /local
 git clone $REPO_URL
-REPO_NAME=$(basename $REPO_URL .git)
 
 # Pull hotelreservation docker images
-docker pull $DOCKER_IMAGE
+su geniuser -c "docker pull $DOCKER_IMAGE"
 
 
 chmod -R 777 /local/DeathStarBench
 chown geniuser -R /local/DeathStarBench
 sudo ln -s /local/DeathStarBench /users/geniuser/
-su geniuser -c "kubectl apply -f /users/geniuser/$REPO_NAME/$KUBERNETES_DIR"
+su geniuser -c "kubectl apply -Rf /users/geniuser/DeathStarBench/$KUBERNETES_DIR"
 
 echo "deathstarbench-k8s setup complete"
 
